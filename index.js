41cfdaff2ce0cd85dfced6f00277d39aa0afa87a
niderman <viktor.niderman@gmail.com>
2023-12-04 16:24:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+import TelegramBot from 'node-telegram-bot-api'
+import locationsMixin from './locationsMixin.js'
+
+const token = ''
+const bot = new TelegramBot(token, { polling: true });
+
+let users = {};
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text
+  const getCurrentLocation = async () => {
+    await bot.sendMessage(chatId, 'Отправь мне свое местоположение', {
+      reply_markup: JSON.stringify({
+        keyboard: [
+          [{ text: 'Отправить геолокацию', request_location: true }],
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      }),
+    })
+  }
+
+  if (!users[chatId]) {
+    // Если пользователь самый первый раз в игре
+    const username = msg?.from?.username ?? 'человек';
+    await bot.sendMessage(chatId, 'Добро пожаловать в игру, ' + username);
+    await bot.sendMessage(chatId, 'Давай для начала проверим, что GPS у тебя работает корректно');
+    users[chatId] = {
+      username: username,
+    }
+  }
+  if (!users[chatId].location && msg.location) {
+    // Если впервые прислал свою локацию
+    users[chatId].location = {
+      latitude: msg.location.latitude,
+      longitude: msg.location.longitude
+    }
+    if (locationsMixin.isClose(locationsMixin.locations.dormitory, users[chatId].location, 200)) {
+      await bot.sendMessage(chatId, 'О, так ты в общаге');
+    }
+    await bot.sendMessage(chatId, 'Отлично, теперь можно начинать');
+  }
+  if (!users[chatId].location) {
+    // Если еще не присылал ни разу локацию
+    await getCurrentLocation();
+    return;
+  }
+})
+
+
+
+
+// bot.on('callback_query', function onCallbackQuery (callbackQuery) {
+//   const action = callbackQuery.data
+//   const msg = callbackQuery.message
+//   const opts = {
+//     chat_id: msg.chat.id,
+//     message_id: msg.message_id,
+//   }
+//
+//   if (action === 'update_info') {
+//     const opts = {
+//       reply_markup: JSON.stringify({
+//         keyboard: [
+//           [{ text: 'Location', request_location: true }],
+//           [{ text: 'Contact', request_contact: true }],
+//         ],
+//         resize_keyboard: true,
+//         one_time_keyboard: true,
+//       }),
+//     }
+//     bot.sendMessage(msg.chat.id, 'Contact and Location request', opts);
+//     bot.deleteMessage(opts)
+//     return;
+//   }
+//   if (action === 'edit') {
+//     text = 'Edited Text'
+//   }
+//
+//   bot.editMessageText(text, opts)
+// })
+//
+//
---
+++ locationsMixin.js
@@ -0,0 +1,36 @@
+const locationsMixin = {
+  dormitory: {
+    latitude: 31.255222,
+    longitude: 34.783896
+  }
+}
+
+function toRadians (degrees) {
+  return degrees * Math.PI / 180
+}
+
+function calculatitudeeDistance (latitude1, longitude1, latitude2, longitude2) {
+  const R = 6371e3
+  const f1 = toRadians(latitude1)
+  const f2 = toRadians(latitude2)
+  const df = toRadians(latitude2 - latitude1)
+  const da = toRadians(longitude2 - longitude1)
+
+  const a = Math.sin(df / 2) * Math.sin(df / 2) +
+    Math.cos(f1) * Math.cos(f2) *
+    Math.sin(da / 2) * Math.sin(da / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  return R * c
+}
+
+function isClose (point1, point2, threshold) {
+  const distance = calculatitudeeDistance(point1.latitude, point1.longitude, point2.latitude,
+    point2.longitude)
+  return distance <= threshold
+}
+
+export default {
+  locations: locationsMixin,
+  isClose,
+}
