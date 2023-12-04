3d8dc8fd498ddeed6e4c00992485e1214677d2f7
niderman <viktor.niderman@gmail.com>
2023-12-04 23:39:37 +0200

    fix style

--- index.js
+++ index.js
@@ -6,68 +6,76 @@ const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN, { polling: true })
 
 let users = {}
 
-const doCurrentStep = async (chatId, msg) => {
-  const currentStep = users[chatId]?.currentStep ?? 0
-  await steps[currentStep](chatId, msg)
+class User {
+  constructor (id) {
+    this.id = id
+    this.currentStep = 0
+    this.testLocationPassed = false
+  }
+
+  askLocation = async (text) => {
+    await bot.sendMessage(this.id, text ?? 'Отправь мне свое местоположение', {
+      reply_markup: JSON.stringify({
+        keyboard: [
+          [{ text: 'Отправить геолокацию', request_location: true }],
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      }),
+    })
+  }
+  sendMessage = async (text) => {
+    await bot.sendMessage(this.id, text)
+  }
+}
+
+const doCurrentStep = async (user, msg) => {
+  await steps[user.currentStep ?? 0](user, msg)
+}
+
+const createNewUser = async (chatId) => {
+  const user = users[chatId] = new User(chatId)
+  await user.sendMessage('Добро пожаловать в игру')
 }
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id
-  await doCurrentStep(chatId, msg)
+  if (!users[chatId]) {
+    await createNewUser(msg.chat.id)
+  }
+  const user = users[chatId]
+  await doCurrentStep(user, msg)
 })
 
 const steps = {
-  0: async (chatId, msg) => {
-    if (!users[chatId]) {
-      const username = msg?.from?.username ?? 'человек'
-      await bot.sendMessage(chatId, 'Добро пожаловать в игру, ' + username)
-      users[chatId] = {
-        username: username,
-        currentStep: 0,
-      }
-      await askLocation(chatId,
+  0: async (user, msg) => {
+    if (!user.testLocationPassed || !msg.location) {
+      await user.askLocation(
         'Давай для начала проверим, что GPS у тебя работает корректно')
       return
     }
-    if (!users[chatId].testLocationPassed && msg.location) {
-      users[chatId].testLocationPassed = true
-      if (locationsMixin.isClose(locationsMixin.locations.dormitory,
-        msg.location, 200)) {
-        await bot.sendMessage(chatId, 'О, так ты в общаге')
-      }
-      await bot.sendMessage(chatId, 'Отлично, теперь можно начинать')
-      await askLocation(chatId, 'Теперь твоя задача дойти до клиты')
-      users[chatId].currentStep = 1
-    } else {
-      await askLocation(chatId)
+    user.testLocationPassed = true
+    if (locationsMixin.isClose(locationsMixin.locations.dormitory,
+      msg.location, 200)) {
+      await user.sendMessage('О, так ты в общаге')
     }
+    await user.sendMessage('Отлично, теперь можно начинать')
+    await user.askLocation('Теперь твоя задача дойти до клиты')
+    user.currentStep = 1
   },
-  1: async (chatId, msg) => {
+  1: async (user, msg) => {
     if (!msg.location) {
-      await askLocation(chatId, 'Пришли мне геолокацию когда ты будешь около клиты')
-      return;
+      await user.askLocation(
+        'Пришли мне геолокацию когда ты будешь около клиты')
+      return
     }
     if (!locationsMixin.isClose(locationsMixin.locations.klita, msg.location,
       300)) {
-      await askLocation(chatId, 'Нет, ты еще далеко')
-    } else {
-      await bot.sendMessage(chatId,
-        'Молодец!!! На этом тестовая версия заканчивается')
+      await user.askLocation('Нет, ты еще далеко')
+      return
     }
-
+    await user.sendMessage('Молодец!!! На этом тестовая версия заканчивается')
   },
-}
-
-const askLocation = async (chatId, text = null) => {
-  await bot.sendMessage(chatId, text ?? 'Отправь мне свое местоположение', {
-    reply_markup: JSON.stringify({
-      keyboard: [
-        [{ text: 'Отправить геолокацию', request_location: true }],
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    }),
-  })
 }
 
 // bot.on('callback_query', function onCallbackQuery (callbackQuery) {
